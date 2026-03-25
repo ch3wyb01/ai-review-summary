551632a1ea4443db9c1c334ca2f159a042855751
Becca Sullivan <beccasullivan30@gmail.com>
2026-03-25 16:31:59 +0000

    add schemas and sample data

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const ReviewSchema = z.object({
+  reviewer: z.string(),
+  stars: z.number().min(1).max(5),
+  review: z.string(),
+  date: z.string(),
+});
+
+export const ProductSchema = z.object({
+  slug: z.string(),
+  name: z.string(),
+  description: z.string(),
+  reviews: z.array(ReviewSchema),
+});
+
+export type Review = z.infer<typeof ReviewSchema>;
+export type Product = z.infer<typeof ProductSchema>;
